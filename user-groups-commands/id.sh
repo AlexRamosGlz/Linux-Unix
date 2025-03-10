2fdb9f5febf5ff3db244db6671130345bdf40e41
AlexRamosGlz <joralexfx@gmail.com>
2025-03-10 09:21:09 -0600

    Notas agregadas para el comando 'id'

---
+++ user-groups-commands/id.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##    --  id [OPTION]... [USER]...  --    ##
+
+  # * Imprime informacion de user y grupo por cada user especificado * #
+
+
+  
+id   # Muestra toda la informacion de usuario   
