4b23181260b13b60b07d94d9cebc42d37681124b
AlexRamosGlz <joralexfx@gmail.com>
2025-03-11 09:19:53 -0600

    Nuevas notas agregadas
    Nuevas notas agregadas para los comandos 'passwd', 'who'
    y 'chage'

--- user-groups-commands/passwd.sh
+++ user-groups-commands/passwd.sh
@@ -4,3 +4,5 @@
 
   # * crea/cambia la contraseña de un USER * #
 
+passwd --stdin $USER_TO_CHANGE_PASSWD    # la opcion '--stdin' permite pasar la contraseña por stdin
+                                         # en lugar de pedirla por consola
---
+++ user-groups-commands/who.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+##       --  who [OPTION]... [ FILE | ARG1 ARG2 ]  --        ##
+
+    # * Muestra quien loggeado en el sistema * #
+
+who -d   # la opcion '-d' muestra la fecha y hora de la ultima vez que el usuario loggeo en el sistema
