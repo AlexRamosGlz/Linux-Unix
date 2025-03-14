695643630a0bcae55accbc1cbb8a75f0528b88ab
AlexRamosGlz <joralexfx@gmail.com>
2025-03-14 12:02:04 -0600

    Notas del comando "su" y "ln" agregadas
    Archivo .gitignore agreado

---
+++ ln-link.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+##    --  ln [OPTION]... [-T] TARGET LINK_NAME --   ##
+
+  # * Crear links entre archivos ( como un acceso directo ) * #  
---
+++ user-groups-commands/su-switch-user.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+##    --  su [options] [-] [user [argument...]] --    ##
+
+su -l   # 
