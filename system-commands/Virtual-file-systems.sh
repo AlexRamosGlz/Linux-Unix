d3be93b1722da9c81f508f9e6aaa36b5d16b3200
AlexRamosGlz <joralexfx@gmail.com>
2025-03-31 18:07:47 +0000

    Nuevas notas agregadas

--- system-commands/Virtual-file-systems.sh
+++ system-commands/Virtual-file-systems.sh
@@ -37,4 +37,48 @@ tree /proc/sys
 
 tree /sys
 
-    # 
+    # El directorio /sys guarda configuracion del kernel en una forma estricta
+    #
+    # NOTA: alguna informacion en duplicada de /proc
+    #
+
+
+
+# Subdirectorios
+
+    # Los subdirectorios comunes son...
+    #
+    #	/block 	-> contiene soft links de todos los block devices dentro de
+    #		   /sys/devices
+    #
+    #	/bus 	-> Contiene subdirectorios por cada bus device en el sistema
+    #
+    #	/class	-> Contiene los devices categorisados por clases
+    #
+    #	/dev	-> devices separados por tipo (block y character) 
+    #
+    #	/devices -> Contiene una herarquia de estructura de devices dentro del 
+    #		    kernel
+    #
+    #	/firmware -> contiene atributos especificos del firmware
+    #
+    #	/fs	-> contiene directorios que representan file systems
+    #
+    #	/kernel	-> informacion acerca del estatus del kernel
+    #
+    #	/module	-> informacion acerca de modulos adicionales que son cargados
+    #		   en el kernel
+    #
+    #	/power	-> contiene parametros del manejo de poder
+
+
+# ejemplos
+
+cat /sys/devices/system/cpu/cpu0/uevent
+	
+	# muestra informacion de nuestro cpu
+
+cat /sys/class/net/eth0/address
+
+	# muestra la direccion MAC de nuestro sistema
+	
