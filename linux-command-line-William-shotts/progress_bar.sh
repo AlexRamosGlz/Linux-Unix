fc933d5982e94d2f9f5cdc09e470353e1770e991
AlexRamosGlz <joralexfx@gmail.com>
2026-09-10 16:58:30 -0600

    progresbar modificado

--- linux-command-line-William-shotts/progress_bar.sh
+++ linux-command-line-William-shotts/progress_bar.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-text="Progreso..."
+text="Progreso...100"
+width=$(tput cols)
 
 calculate_text_position () {
 
-	test wc -c $text -eq 0 && text_position=0
+	[ "$(wc -c <<< "$text")" -eq 0 ] && text_position=0
 
 	text_position=$(echo "($(tput cols)/2)-(15/2)" | bc)
 }
 
 
 draw_bar() {
-
-	for i in $(seq 1 100); do
-		printf "\e[112CProgreso...%d" "$i"
-		
-		for ((j=0;j<i;j++)); do
-			printf "#"
-			sleep 0.5
-		done
+	printf "[\e[999C]\r\e[$((text_position))C$text\r\e[1C\e[s"
+	for i in $(seq 2 $(tput cols)); do
+		range="{$i..236}"
+		if (( i >= (text_position - 1) ||  i >= text_position + 16 )); then
+			printf "\e[$((i))C"	 				
+		else  	
+	 		printf "#%.0s\e[s" $range
+			sleep 0.09
+		fi	
 	done
 }
 
 
 calculate_text_position
 draw_bar
-
